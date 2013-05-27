580e154898a70200d5bd0f5c1ef6002b99e8fa99
Christian Gambardella <hi@nerdyglass.es>
2013-05-27 09:07:51 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CarrierwaveBackgrounderMongoid::Application.config.secret_token = '65f1fff8e3ea65477dc7975013566e976a9c8328ede108766d5fb44bc1b588865fc206d12fc6ebe50f6ee0edc7a2fd23ebc755e6f66bd2cc458010856b72075a'
